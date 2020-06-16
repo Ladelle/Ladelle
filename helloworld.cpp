0bea68e74ba70bd397a406261cbdff34de4500b6
LaDelle Augustine <ladelle2016@gmail.com>
2020-06-16 09:43:25 -0500

    added a hello world file

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+/* This is a simple C++ Hello World File */
+/* 06/ 16/2020 */
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main()
+{
+
+cout << "HeLlO wOrLd "<< endl;
+
+system("pause");
+return 0; 
+}
